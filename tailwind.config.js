47485f6f084d0769343bc9a4f48ac48d7f7c968c
Sérgio Nunes <sagnunes@gmail.com>
2024-02-20 20:21:15 +0000

    added style guide to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "1440px",
+    },
+    extend: {
+      colors: {
+        red: {
+          desaturated: "hsl(0, 36%, 70%)",
+          soft: "hsl(0, 93%, 68%)",
+          darkGrayish: "hsl(0, 6%, 24%)",
+        },
+      },
+      backgroundImage: {
+        "soft-pink-gradient":
+          "linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 80%, 86%))",
+        "darker-pink-gradient":
+          "linear-gradient(135deg,hsl(0, 80%, 86%), hsl(0, 74%, 74%))",
+      },
+    },
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
